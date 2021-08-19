d35b4dd0753e3f45359afa9bedf18dd544d7629d
umrepos <88673307+umrepos@users.noreply.github.com>
2021-08-19 10:28:44 -0500

    Update compra.php

--- site/compra.php
+++ site/compra.php
@@ -71,7 +71,15 @@
         <form method="post" action="transacción.php" id="contactfrm" role="form">
           <div class="col-sm-12">
             <div class="form-group">
-              <label for="producto">Producto: <?php echo $_GET["producto"] ?> </label>
+              <label for="producto">Elija un producto </label>
+	      <select id="producto" name="producto">		    
+                    <option value="Mascarilla KN95">Mascarilla KN95</option>
+		    <option value="Gel Antibacterial Escudo Sobre">Gel Antibacterial Escudo Sobre</option>
+		    <option value="Spray Desinfectante">Spray Desinfectante</option>
+		    <option value="Best Trading Cubrebocas">Best Trading Cubrebocas</option>
+		    <option value="Careta Protectora PVC">Careta Protectora PVC</option>		    
+	      </select>
+		    
             </div>
             <div class="form-group">
               <label for="cantidad">Cantidad</label>
@@ -96,9 +104,39 @@
 	    <div class="form-group">
                 <label for="ciudad">Elija la ciudad:</label>
                 <select id="ciudad" name="ciudad">
-                  <option value="Huasco">Huasco</option>
-                  <option value="Mérida">Mérida</option>
-                  <option value="Zapopan">Zapopan</option>
+		  <option value="no">Seleccione una...</option>
+                  <option value="Aguascalientes">Aguascalientes</option>
+      <option value="Baja California">Baja California</option>
+      <option value="Baja California Sur">Baja California Sur</option>
+      <option value="Campeche">Campeche</option>
+      <option value="Chiapas">Chiapas</option>
+      <option value="Chihuahua">Chihuahua</option>
+      <option value="CDMX">Ciudad de México</option>
+      <option value="Coahuila">Coahuila</option>
+      <option value="Colima">Colima</option>
+      <option value="Durango">Durango</option>
+      <option value="Estado de México">Estado de México</option>
+      <option value="Guanajuato">Guanajuato</option>
+      <option value="Guerrero">Guerrero</option>
+      <option value="Hidalgo">Hidalgo</option>
+      <option value="Jalisco">Jalisco</option>
+      <option value="Michoacán">Michoacán</option>
+      <option value="Morelos">Morelos</option>
+      <option value="Nayarit">Nayarit</option>
+      <option value="Nuevo León">Nuevo León</option>
+      <option value="Oaxaca">Oaxaca</option>
+      <option value="Puebla">Puebla</option>
+      <option value="Querétaro">Querétaro</option>
+      <option value="Quintana Roo">Quintana Roo</option>
+      <option value="San Luis Potosí">San Luis Potosí</option>
+      <option value="Sinaloa">Sinaloa</option>
+      <option value="Sonora">Sonora</option>
+      <option value="Tabasco">Tabasco</option>
+      <option value="Tamaulipas">Tamaulipas</option>
+      <option value="Tlaxcala">Tlaxcala</option>
+      <option value="Veracruz">Veracruz</option>
+      <option value="Yucatán">Yucatán</option>
+      <option value="Zacatecas">Zacatecas</option>
                 </select>
             </div>
             <div class="form-group">
