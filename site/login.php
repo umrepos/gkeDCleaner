9def2fa87b2b39b7dd39d399f7c6f040f93462f8
loluna1 <88747795+loluna1@users.noreply.github.com>
2021-08-19 10:00:41 -0500

    Update login.php

--- site/login.php
+++ site/login.php
@@ -68,7 +68,7 @@
         </div>
       </div>
       <div class="row mrgn30">
-        <form method="post" action="compra.php" id="contactfrm" role="form">
+        <form method="post" action="index.html" id="contactfrm" role="form">
           <div class="col-sm-12">
             <div><h2>Inicio de sesión</h2></div>
             <div class="form-group">
